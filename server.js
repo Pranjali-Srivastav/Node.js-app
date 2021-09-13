df6d483ed328c2a531276c42ac7b17b1bd2a25c0
Pranjali Srivastav <pranjalisrivastav9@gmail.com>
2021-09-13 17:05:02 +0530

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:orange;">Welcome to my first Node.js project....!!</h1> \n');
+  res.send('<h1 style="color:orange;">Welcome to my first Node.js project..!</h1> \n');
 });
 
 app.listen(PORT, HOST);
